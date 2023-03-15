c8acb7f49c98aa1b506fedbedaff01ab9abf9274
zayfaali <huzaifaalix@gmail.com>
2023-03-15 13:33:09 +0500

    haha

--- server.js
+++ server.js
@@ -81,7 +81,7 @@ app.post("/register", (req, res) => {
       })
       .then(trx.commit)
       .catch(trx.rollback);
-  }).catch((err) => res.status(400).json("unable to register"));
+  }).catch((err) => res.status(400).json("unable to register hahaha"));
 });
 
 app.get("/profile/:id", (req, res) => {
